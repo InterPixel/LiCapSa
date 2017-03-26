ff4d89446f9a130b793961b0208e9830e328611d
Stanley Heryanto <stanleyheryanto@Gmail.com>
2017-03-26 21:56:17 +0700

    apalah ini

---
+++ app/src/main/java/com/InterPixel/LiCapSa/MainMenuActivity.java
@@ -0,0 +1,28 @@
+package com.InterPixel.LiCapSa;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class MainMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main_menu);
+
+    }
+
+    public void startGameActivity(View view){
+        Intent intent = new Intent(this,GameActivity.class);
+        startActivity(intent);
+    }
+}
