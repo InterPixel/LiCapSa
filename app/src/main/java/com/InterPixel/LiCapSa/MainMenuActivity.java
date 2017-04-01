828dd416d5dfe429739649ff8c26b64b7933edbe
Stanley Heryanto <stanleyheryanto@Gmail.com>
2017-04-01 21:03:51 +0700

    Sign In
    Also keystore

--- app/src/main/java/com/InterPixel/LiCapSa/MainMenuActivity.java
+++ app/src/main/java/com/InterPixel/LiCapSa/MainMenuActivity.java
@@ -19,11 +19,26 @@ public class MainMenuActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_menu);
 
+        PlayGames.fap().initialize(this);
+
+
     }
 
     public void startGameActivity(View view){
         Intent intent = new Intent(this,GameActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void signIn(View view) {
+        PlayGames.fap().SignIn();
+    }
+
+    public void signOut(View view) {
+        PlayGames.fap().SignOut();
+    }
+
+    public void invitePlayers(View view){
+        PlayGames.fap().invitePlayers();
     }
 }
--- app/src/main/java/com/InterPixel/LiCapSa/PlayGames.java
+++ app/src/main/java/com/InterPixel/LiCapSa/PlayGames.java
@@ -3,14 +3,18 @@ package com.InterPixel.LiCapSa;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesStatusCodes;
 import com.google.android.gms.games.multiplayer.Invitation;
+import com.google.android.gms.games.multiplayer.Multiplayer;
 import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
 import com.google.android.gms.games.multiplayer.Participant;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
@@ -24,13 +28,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class PlayGames extends GameActivity implements GoogleApiClient.ConnectionCallbacks,
+public class PlayGames extends Activity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, RealTimeMessageReceivedListener,
         RoomStatusUpdateListener, RoomUpdateListener, OnInvitationReceivedListener{
 
     /*
        API Implementation only, please don't refer to API directly, use methods defined HERE
        Invoke with singleton
+       Breaching rules will result in system instability
+
+       In short, there will be these multiplayer setups
+       1. Automatching which match a player with random people
+            -Always 4 people plays
+       2. Invite which player can choose who he plays with
+            -Incase player < 4, AI will fill in the gaps
+            -All AIs are from whoever assigned player 1
+       3. Accepting from inbox where invited player joins the match
 
 
     */
@@ -49,9 +62,17 @@ public class PlayGames extends GameActivity implements GoogleApiClient.Connectio
 
     //Variables
     ArrayList<Participant> participants = null;
-    boolean isMultiplayer = false;
+    static boolean isMultiplayer = false;
     Intent intent;
-    boolean GameMode; //false for normal
+    final static int VARIANT_NORMAL = 888;
+    final static int VARIANT_GOSOK = 666;
+    static int gamemodes = VARIANT_NORMAL;
+    final static int MAX_OPPONENTS = 3, MIN_OPPONENTS = 1;
+    final static int RC_SELECT_PLAYERS = 10000;
+    final static int RC_INVITATION_INBOX = 10001;
+    final static int RC_WAITING_ROOM = 10002;
+    final static int RC_SIGN_IN = 10003;
+    Activity activity;
 
     //constructor
     private void PlayGames(){
@@ -62,21 +83,37 @@ public class PlayGames extends GameActivity implements GoogleApiClient.Connectio
     private GoogleApiClient apiClient;
 
     //Building Client, call this method first before using anything else here
-    public void initialize(){
-         apiClient = new GoogleApiClient.Builder(this)
-                 .addApi(Games.API)
-                 .addScope(Games.SCOPE_GAMES)
+    public void initialize(Activity activity){
+        debug("Attempt to build client API");
+        this.activity = activity;
+         apiClient = new GoogleApiClient.Builder(activity)
                  .addOnConnectionFailedListener(this)
                  .addConnectionCallbacks(this)
+                 .addApi(Games.API)
+                 .addScope(Games.SCOPE_GAMES)
                  .build();
+        debug("Client API built");
     }
 
     public void SignIn(){
-        apiClient.connect();
+        debug("Attempt to sign in");
+        if(!isConnected()){
+            apiClient.connect();
+        }else{
+            debug("Already signed in");
+        }
     }
 
     public void SignOut(){
-        apiClient.disconnect();
+        debug("Signing out");
+        if(isConnected()){
+            Games.signOut(apiClient);
+            apiClient.disconnect();
+            debug("Signed out");
+        }else{
+            debug("Not signed in");
+        }
+
     }
 
     public boolean isConnected(){
@@ -86,11 +123,13 @@ public class PlayGames extends GameActivity implements GoogleApiClient.Connectio
     @Override
     public void onConnected(Bundle bundle){
         //sign in sucessful
+        debug("Sucessfully logged in");
     }
 
     @Override
     public void onConnectionSuspended(int i) {
         //reconecting
+        debug("Connection problem");
         apiClient.connect();
     }
 
@@ -98,18 +137,119 @@ public class PlayGames extends GameActivity implements GoogleApiClient.Connectio
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         //failed to sign in
+        debug("Failed to sign in : " + connectionResult);
+
+        if ( connectionResult.hasResolution() ){
+            try {
+                connectionResult.startResolutionForResult(activity, RC_SIGN_IN);
+                debug("Resolution ran");
+            } catch (IntentSender.SendIntentException e) {
+                debug("Exception when resolving signing in " + e);
+                //just give up on signing in
+            }
+        }
+
     }
 
-    void startQuickGame() {
-        final int MIN_OPPONENTS = 1, MAX_OPPONENTS = 3;
-        Bundle autoMatchCriteria = RoomConfig.createAutoMatchCriteria(MIN_OPPONENTS,
-                MAX_OPPONENTS, 0);
-        RoomConfig.Builder rtmConfigBuilder = RoomConfig.builder(this);
-        rtmConfigBuilder.setMessageReceivedListener(this);
-        rtmConfigBuilder.setRoomStatusUpdateListener(this);
-        rtmConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+    void automatch() {
+        Bundle autoMatchCriteria = RoomConfig.createAutoMatchCriteria(MAX_OPPONENTS,
+                MAX_OPPONENTS, gamemodes);
+        RoomConfig.Builder roomConfigBuilder = RoomConfig.builder(this);
+        roomConfigBuilder.setMessageReceivedListener(this);
+        roomConfigBuilder.setRoomStatusUpdateListener(this);
+        roomConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
         keepScreenOn();
-        Games.RealTimeMultiplayer.create(apiClient, rtmConfigBuilder.build());
+        Games.RealTimeMultiplayer.create(apiClient, roomConfigBuilder.build());
+    }
+
+    void showInvitation(){
+        intent = Games.Invitations.getInvitationInboxIntent(apiClient);
+        activity.startActivityForResult(intent,RC_INVITATION_INBOX);
+    }
+
+    void invitePlayers(){
+        intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(apiClient, MIN_OPPONENTS, MAX_OPPONENTS);
+        activity.startActivityForResult(intent, RC_SELECT_PLAYERS);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent resultIntent){
+
+        RoomConfig.Builder roomConfigBuilder;
+
+        switch (requestCode){
+            case RC_SELECT_PLAYERS:
+
+                if ( responseCode != Activity.RESULT_OK ){
+                    debug("Select player UI cancelled");
+                    return;
+                }
+
+                debug("Select player UI suceeded");
+
+                //get the invitee list
+                final ArrayList<String> invitedPeople = resultIntent.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+                debug("Invited people = " + invitedPeople.size());
+
+                // get auto-match criteria
+                Bundle autoMatchCriteria = null;
+                int minAutoMatchPlayers =
+                        resultIntent.getIntExtra(Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+                int maxAutoMatchPlayers =
+                        resultIntent.getIntExtra(Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+                if (minAutoMatchPlayers > 0) {
+                    autoMatchCriteria = RoomConfig.createAutoMatchCriteria(minAutoMatchPlayers, maxAutoMatchPlayers, gamemodes);
+                } else {
+                    autoMatchCriteria = null;
+                }
+
+                //Create the room
+                roomConfigBuilder = RoomConfig.builder(this)
+                        .setRoomStatusUpdateListener(this)
+                        .setMessageReceivedListener(this)
+                        .addPlayersToInvite(invitedPeople)
+                        .setVariant(gamemodes);
+                if ( autoMatchCriteria != null ){
+                    roomConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+                }
+                RoomConfig roomConfig = roomConfigBuilder.build();
+                Games.RealTimeMultiplayer.create(apiClient, roomConfig);
+
+                keepScreenOn();
+
+                break;
+
+            case RC_INVITATION_INBOX:
+                if ( responseCode != Activity.RESULT_OK ){
+                    debug("Invitation inbox UI cancelled " + responseCode);
+                    return;
+                }
+                debug("Invitation inbox UI suceeded");
+
+                Invitation inv = resultIntent.getExtras().getParcelable(Multiplayer.EXTRA_INVITATION);
+
+                roomConfigBuilder = RoomConfig.builder(this)
+                        .setInvitationIdToAccept(inv.getInvitationId())
+                        .setRoomStatusUpdateListener(this)
+                        .setMessageReceivedListener(this)
+                        .setVariant(inv.getVariant());
+                gamemodes = inv.getVariant();
+                keepScreenOn();
+                Games.RealTimeMultiplayer.create(apiClient, roomConfigBuilder.build());
+
+                break;
+            case RC_WAITING_ROOM:
+                break;
+
+            case RC_SIGN_IN:
+                debug("Resolution ends");
+                if(responseCode == Activity.RESULT_OK){
+                    debug("Attempt to reconnect again");
+                    apiClient.connect();
+                }
+                break;
+        }
     }
 
     //Keeping screen on while matching to prevent errors
@@ -198,12 +338,23 @@ public class PlayGames extends GameActivity implements GoogleApiClient.Connectio
     }
 
     @Override
-    public void onRoomCreated(int i, Room room) {
-
+    public void onRoomCreated(int statusCode, Room room) {
+        if(statusCode != GamesStatusCodes.STATUS_OK){
+            stopKeepingScreenOn();
+            debug("Error creating room");
+        }else{
+            debug("Room created");
+        }
     }
 
     @Override
-    public void onJoinedRoom(int i, Room room) {
+    public void onJoinedRoom(int statusCode, Room room) {
+        if(statusCode != GamesStatusCodes.STATUS_OK){
+            stopKeepingScreenOn();
+            debug("Error joining room");
+        }else{
+            debug("Room joined");
+        }
 
     }
 
@@ -213,13 +364,22 @@ public class PlayGames extends GameActivity implements GoogleApiClient.Connectio
     }
 
     @Override
-    public void onRoomConnected(int i, Room room) {
-
+    public void onRoomConnected(int statusCode, Room room) {
+        if(statusCode != GamesStatusCodes.STATUS_OK){
+            stopKeepingScreenOn();
+            debug("Error connecting to room");
+        }else{
+            debug("Connected to room");
+        }
     }
 
     public void invite(boolean gamemode){
         intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(apiClient, 1, 3);
         startActivity(intent);
+    }
+
+    public void debug(String string){
+        Log.d("LiCapSa", "debug: " + string);
     }
 
 
